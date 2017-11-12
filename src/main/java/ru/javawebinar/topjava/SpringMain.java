f41d78772d8e0b94dd4b8291adcf68aa78262554
VadimSimonov <dark-vadim@mail.ru>
2017-11-12 08:35:41 +0300

    HW2_1 begin

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -14,7 +14,15 @@ public class SpringMain {
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userName1", "email1", "password1", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userName2", "email2", "password2", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userName3", "email3", "password3", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userName4", "email4", "password4", Role.ROLE_ADMIN));
+
+            adminUserController.create(new User(null, "userName5", "email5", "password5", Role.ROLE_USER));
+
+            System.out.println(adminUserController.getAll().size());
+
         }
     }
 }

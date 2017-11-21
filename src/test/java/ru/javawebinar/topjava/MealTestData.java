8c4c0f4dc65284a39d8cf4a1d06d42234ce37a15
VadimSimonov <dark-vadim@mail.ru>
2017-11-21 16:56:04 +0300

    HW3 Optional begin

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -7,8 +7,11 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class MealTestData {
+    public static final int USER_ID = START_SEQ;
+    public static final int ID = START_SEQ + 1;
 
     public static final Meal meal1 = new Meal(1,LocalDateTime.now(),"meal1",500);
     public static final Meal meal2 = new Meal(2,LocalDateTime.now(),"meal2",400);
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
@@ -17,8 +19,17 @@ import static org.junit.Assert.*;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
 
+    static {
+        // Only for postgres driver logging
+        // It uses java.util.logging and logged via jul-to-slf4j bridge
+        SLF4JBridgeHandler.install();
+    }
+    @Autowired
+    private MealService service;
+
     @Test
     public void get() throws Exception {
+
     }
 
     @Test

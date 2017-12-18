a7d77cd61598865b781c08a0fc9881d042c28bde
VadimSimonov <dark-vadim@mail.ru>
2017-12-18 15:31:03 +0300

    HW6 springMVC getBetweenFilter complete

--- src/main/java/ru/javawebinar/topjava/web/MealController.java
+++ src/main/java/ru/javawebinar/topjava/web/MealController.java
@@ -1,20 +1,29 @@
 package ru.javawebinar.topjava.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Controller
 public class MealController {
@@ -22,7 +31,7 @@ public class MealController {
     @Autowired
     MealService mealService;
 
-    @RequestMapping(value = "meals",method = RequestMethod.GET)
+    @GetMapping("/meals")
     public ModelAndView getAll(ModelAndView modelAndView)
     {
         int id=AuthorizedUser.id();
@@ -31,4 +40,20 @@ public class MealController {
         modelAndView.addObject("meals", m);
         return modelAndView;
     }
+
+    @PostMapping("/meals")
+    public ModelAndView getBeetween(ModelAndView modelAndView,
+                                    @RequestParam (value ="startDate" , required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                                    @RequestParam (value ="endDate" , required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+                                    @RequestParam (value ="startTime" , required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime startTime,
+                                    @RequestParam (value ="endTime" , required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime endTime)
+    {
+        int id=AuthorizedUser.id();
+        List<Meal> list = mealService.getBetweenDates(startDate != null ? startDate : DateTimeUtil.MIN_DATE, startDate != null ? startDate : DateTimeUtil.MAX_DATE, id);
+        List<MealWithExceed> result = MealsUtil.getFilteredWithExceeded(list, startTime, endTime, MealsUtil.DEFAULT_CALORIES_PER_DAY);
+        modelAndView.addObject("meals", result);
+        return modelAndView;
+    }
+
+
 }

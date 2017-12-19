9c54421230f22da3a110b668dffc2273bace083d
VadimSimonov <dark-vadim@mail.ru>
2017-12-19 17:58:27 +0300

    HW6 springMVC update and delete complete

--- src/main/java/ru/javawebinar/topjava/web/MealController.java
+++ src/main/java/ru/javawebinar/topjava/web/MealController.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.javawebinar.topjava.AuthorizedUser;
@@ -13,17 +12,10 @@ import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 @Controller
 public class MealController {
@@ -49,11 +41,42 @@ public class MealController {
                                     @RequestParam (value ="endTime" , required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime endTime)
     {
         int id=AuthorizedUser.id();
-        List<Meal> list = mealService.getBetweenDates(startDate != null ? startDate : DateTimeUtil.MIN_DATE, startDate != null ? startDate : DateTimeUtil.MAX_DATE, id);
+        List<Meal> list = mealService.getBetweenDates(startDate != null ? startDate : DateTimeUtil.MIN_DATE, endDate != null ? endDate : DateTimeUtil.MAX_DATE, id);
         List<MealWithExceed> result = MealsUtil.getFilteredWithExceeded(list, startTime, endTime, MealsUtil.DEFAULT_CALORIES_PER_DAY);
         modelAndView.addObject("meals", result);
         return modelAndView;
     }
 
+    @GetMapping("/meals/delete/{id}")
+    public ModelAndView delete(@PathVariable(name = "id") Integer id)
+    {
+        mealService.delete(id,AuthorizedUser.id());
+        return new ModelAndView("redirect:/meals");
+    }
 
+    @GetMapping("/meals/update/{id}")
+    public ModelAndView Update(@PathVariable(name = "id") Integer id)
+    {
+        Meal user = mealService.getWithUser(id, AuthorizedUser.id());
+        return new ModelAndView("mealForm", "meal", user);
+    }
+
+    @PostMapping("/meals/save")
+            public ModelAndView createAndUpdate(
+                                                @RequestParam (value = "id" , required = false) Integer id,
+                                                @RequestParam (value = "dateTime",required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime,
+                                                @RequestParam (value = "description",required = false)String description,
+                                                @RequestParam (value = "calories",required = false)Integer calories)
+    {
+
+        if (id==null)
+        {
+            Meal meal = new Meal(localDateTime, description, calories);
+            mealService.create(meal,AuthorizedUser.id());
+        }else {
+            Meal meal = new Meal(id,localDateTime, description, calories);
+            mealService.update(meal,AuthorizedUser.id());
+        }
+        return new ModelAndView("redirect:/meals");
+    }
 }
